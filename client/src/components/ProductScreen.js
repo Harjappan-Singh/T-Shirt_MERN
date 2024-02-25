16d1204edd08692b8938de787a596e1e217fb3b2
Harjappan Singh <singhharjappan@gmail.com>
2024-02-25 17:07:28 +0000

    combined cart functionality into product details

--- client/src/components/ProductScreen.js
+++ client/src/components/ProductScreen.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import Rating from './Rating';
 import '../css/Product.css';
 import HeartIcon from './HeartIcon';
-import { Link } from "react-router-dom";
-import { ACCESS_LEVEL_ADMIN, SERVER_HOST } from "../config/global_constants";
+import { Link } from 'react-router-dom';
+import { ACCESS_LEVEL_ADMIN, SERVER_HOST } from '../config/global_constants';
 
 class ProductScreen extends Component {
   render() {
@@ -11,19 +11,35 @@ class ProductScreen extends Component {
     return (
       <div className="product-card">
         <div className="product-image">
-          <a href={`/product/${product.slug}`}>
+          <a href={`/product/${product._id}`}>
             <img src={product.productImage} alt={product.name} />
             <HeartIcon />
           </a>
         </div>
         <div className="product-info">
-          <a href={`/product/${product.slug}`} className="product-name">
+          <a href={`/product/${product._id}`} className="product-name">
             {product.name}
           </a>
           <Rating rating={product.rating} numReviews={product.numReviews} />
           <div className="product-price">€{product.price}</div>
-         <div> {localStorage.accessLevel >= ACCESS_LEVEL_ADMIN ? <Link className="red-button" to={"/DeleteTshirt/" + this.props.product._id}>Delete</Link> : null}
-                {localStorage.accessLevel >= ACCESS_LEVEL_ADMIN  ? <Link className="green-button" to={"/EditTshirt/" + this.props.product._id}>Edit</Link> : null}
+          <div>
+            {' '}
+            {localStorage.accessLevel >= ACCESS_LEVEL_ADMIN ? (
+              <Link
+                className="red-button"
+                to={'/DeleteTshirt/' + this.props.product._id}
+              >
+                Delete
+              </Link>
+            ) : null}
+            {localStorage.accessLevel >= ACCESS_LEVEL_ADMIN ? (
+              <Link
+                className="green-button"
+                to={'/EditTshirt/' + this.props.product._id}
+              >
+                Edit
+              </Link>
+            ) : null}
           </div>
         </div>
       </div>
